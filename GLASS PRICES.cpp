9aa6bbca1ba6f07de5146ce809be1fc0bd394f52
Prince Kumar Gupta <princelj11e@gmail.com>
2024-10-23 21:21:59 +0530

    Add files via upload

---
+++ GLASS PRICES.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+  int X;
+  int Y;
+  cin>> X;
+  cin>> Y;
+  if (Y <= (2*X)){
+    cout<< "METAL"<<endl;
+  }else{
+    cout<< "PLASTIC"<<endl;
+  }
+}
