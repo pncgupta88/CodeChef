862d8e8db06a024373ef976f48baea7a27ff67ae
Prince Kumar Gupta <princelj11e@gmail.com>
2024-12-29 23:55:38 +0530

    Add files via upload

---
+++ hollowNumRectPatt.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int r,c;
+    cin>>r>>c;
+
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            if (i==1 || i==r || j==1 || j==c)
+            {
+                cout<< j;
+            }else
+            {
+                cout<< " ";
+            }                     
+        }
+        cout<<endl;      
+    }
+    return 0;
+}
